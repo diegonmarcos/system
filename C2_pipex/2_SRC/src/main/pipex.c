feffa209814007dd93d6a955779db75c5e26c0d9
diegonmarcos <diegonmarcos@gmail.com>
2025-02-27 13:59:46 +0100

    up

--- C2_pipex/2_SRC/src/main/pipex.c
+++ C2_pipex/2_SRC/src/main/pipex.c
@@ -6,12 +6,28 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:30:19 by dinepomu          #+#    #+#             */
-/*   Updated: 2025/02/11 16:16:44 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/02/27 13:38:47 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+/** @brief 
+ *
+ * 	@details
+ * 	@par Details:
+ * 	- 
+ * 	- 
+ * 	- 
+ * 	- 
+ * 	-  
+ * 
+ * 	@par Usage:
+ * 	- @b [parm1] is
+ * 	- @b [parm2] is
+ * 	- @b [parm3] is
+ * 	- @b return is
+ */
 int	main(int argc, char **argv, char **env)
 {
 	error_handling_pipex(argc);
