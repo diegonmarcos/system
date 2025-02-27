a0a4c3afff9f9c522e7c0c9f792c81dd28591b1f
diegonmarcos <diegonmarcos@gmail.com>
2025-02-27 16:01:43 +0100

    ft_set_pipe_flow

--- C2_pipex/2_SRC/src/main/pipex.c
+++ C2_pipex/2_SRC/src/main/pipex.c
@@ -6,27 +6,35 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:30:19 by dinepomu          #+#    #+#             */
-/*   Updated: 2025/02/27 13:38:47 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/02/27 14:29:00 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-/** @brief 
+/** @brief The pipex project is a simplified implementation of a Unix pipeline.
  *
  * 	@details
  * 	@par Details:
- * 	- 
- * 	- 
- * 	- 
- * 	- 
- * 	-  
+ * 	Reads input from a file, processes this input through a series of commands,
+ * 	and then writes the processed output to another file. This project mimics the
+ * 	behavior of Unix pipelines, allowing for the chaining of commands where the 
+ * 	output of one command becomes the input for the next. The project is 
+ *	designed to be run from the command line, utilizing command-line arguments
+ *	and environment variables to specify the input file, commands, 
+ *	and output file.
  * 
  * 	@par Usage:
- * 	- @b [parm1] is
- * 	- @b [parm2] is
- * 	- @b [parm3] is
- * 	- @b return is
+ * 	- @b [parm1] argc: The number of command-line arguments.
+ * 	- @b [parm2] argv: An array of command-line arguments.
+ * 	- @b [parm3] env: An array of environment variables.
+ * 	- @b return: Returns 0 on successful execution.
+ * 
+ * 	@par Example:
+ * 	@code
+ * 	env > infile.txt
+ *	./pipex infile.txt "grep -x "PATH=.*"" "cat" outfile.txt
+ * 	@endcode
  */
 int	main(int argc, char **argv, char **env)
 {
--- C2_pipex/2_SRC/src/pipes/1.ft_piping.c
+++ C2_pipex/2_SRC/src/pipes/1.ft_piping.c
@@ -6,33 +6,33 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:30:19 by dinepomu          #+#    #+#             */
-/*   Updated: 2025/02/11 16:18:17 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:00:49 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	child(char **cmd, int *pipe_fd, char **env)
-{
-	int	fd;
+static void	set_pipe_flow(int pid, char **cmd, int *pipe_fd, char **env);
+static void	data_client(char **cmd, char **env);
+static void	data_server(char **cmd, char **env);
 
-	fd = ft_open_readorwrite(cmd[1], 0);
-	dup2(fd, 0);
-	dup2(pipe_fd[1], 1);
-	close(pipe_fd[0]);
-	ft_execve(cmd[2], env);
-}
-
-static void	parent(char **cmd, int *pipe_fd, char **env)
-{
-	int	fd;
-
-	fd = ft_open_readorwrite(cmd[4], 1);
-	dup2(fd, 1);
-	dup2(pipe_fd[0], 0);
-	close(pipe_fd[1]);
-	ft_execve(cmd[3], env);
-}
+/** @brief Implementation of a Unix pipeline.
+ *
+ * 	@details
+ * 	@par Details:
+ * 	- pipe_fd[2] contains the file descriptors for the pipe,
+ * 		[0] for reading [1] for writing.
+ * 	- ft_fork() creates a parente and a child process that runs concurrently.
+ * 	- parent() sees PID != 0 and will read from the pipe and write output file.
+ * 	- child() sees PID = 0 and will execute the first command.
+ * 	-  0 STD_IN, 1 STD_OUT, 2 STD_ERR.
+ *  - dup2(fd, fd2) will duplicate the file descriptor fd to fd2, 
+ * 		and will close fd2 if open.
+ * 
+ * 	@par Usage:
+ * 	- @b [parm1] is a list of commdands
+ * 	- @b [parm2] is a list of environment variables
+ */
 
 void	ft_piping(char **cmd, char **env)
 {
@@ -41,8 +41,39 @@ void	ft_piping(char **cmd, char **env)
 
 	ft_pipe(pipe_fd);
 	pid = ft_fork();
-	if (!pid)
-		child(cmd, pipe_fd, env);
+	set_pipe_flow(pid, cmd, pipe_fd, env);
+	if (pid)
+		data_client(cmd, env);
 	else
-		parent(cmd, pipe_fd, env);
+		data_server(cmd, env);
+}
+
+static void	set_pipe_flow(int pid, char **cmd, int *pipe_fd, char **env)
+{
+	int	fd;
+
+	if (pid)
+	{
+		close(pipe_fd[0]);
+		fd = ft_open_readorwrite(cmd[1], 0);
+		dup2(fd, 0);
+		dup2(pipe_fd[1], 1);
+	}
+	else
+	{
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], 0);
+		fd = ft_open_readorwrite(cmd[4], 1);
+		dup2(fd, 1);
+	}
+}
+
+static void	data_client(char **cmd, char **env)
+{
+	ft_execve(cmd[2], env);
+}
+
+static void	data_server(char **cmd, char **env)
+{
+	ft_execve(cmd[3], env);
 }
