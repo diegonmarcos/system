eb047117c38eca2aa9a923a6c62d25782176ed03
diegonmarcos <diegonmarcos@gmail.com>
2025-03-01 08:07:44 +0100

    up

---
+++ S1_docker/make.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# if ./make install, runs ./make_install.sh
+# if ./make run, runs ./make_run.sh
+
+case "$1" in
+	install)
+		./make_install.sh
+		;;
+	run)
+		./make_run.sh
+		;;
+	*)
+		echo "Invalid argument"
+		exit 1
+		;;
+esac
+
+if [ $# -eq help ]; then
+	echo "install or run are the only valid arguments"
+	echo "install: to install the container"
+	echo "run: to run the container"
+	echo "./make.sh install"
+	echo "./make.sh run"
+	exit 1
+fi
