34007eed36fa25e7a565c2f84443b91bc512c137
diegonmarcos <diegonmarcos@gmail.com>
2025-03-02 13:09:25 +0100

    docker

--- S1_docker/make.sh
+++ S1_docker/make.sh
@@ -1,26 +1,29 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    make.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/03/02 12:30:02 by dinepomu          #+#    #+#              #
+#    Updated: 2025/03/02 13:06:42 by dinepomu         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/bash
 
-# if ./make install, runs ./make_install.sh
-# if ./make run, runs ./make_run.sh
+######**BUILD AND RUN THE IMAGE**######
+### Build the docker image
+docker build -t push_swap_image .
 
-case "$1" in
-	install)
-		./src/make_install.sh
-		;;
-	run)
-		./src/make_run.sh
-		;;
-	*)
-		echo "Invalid argument"
-		exit 1
-		;;
-esac
+### Run the docker image
+#docker run push_swap_image
+docker run -it
+	-v /push_swap:/push_swap
+	push_swap_image
+	bash
 
-if [ $# -eq help ]; then
-	echo "install or run are the only valid arguments"
-	echo "install: to install the container"
-	echo "run: to run the container"
-	echo "./make.sh install"
-	echo "./make.sh run"
-	exit 1
-fi
+######**INTERACTION INSIDE OF IT**######
+### Run the push_swap program (as with -it flag we are inside the container)
+#docker run push_swap_image
+make "$@"
