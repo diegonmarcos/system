52a536f99eb6a5b2ee8c8a2c5b446f8212bbef5d
Diego <diegonmarcos@gmail.com>
2025-03-25 16:13:01 +0100

    up

--- C2_pipex/2_SRC/src/shell/2.ft_execve.c
+++ C2_pipex/2_SRC/src/shell/2.ft_execve.c
@@ -6,7 +6,7 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:30:19 by dinepomu          #+#    #+#             */
-/*   Updated: 2025/03/12 15:50:47 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/03/25 16:12:51 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,17 @@
 char	*ft_my_getenv_(char *cmd, char **env);
 char	*ft_my_getenv(char *name, char **env);
 
+/**
+ * ft_execve - Executes a command using execve system call.
+ * @cmd: The command to be executed.
+ * @env: The environment variables.
+ *
+ * This function splits the command string into an array of arguments,
+ * retrieves the executable path from the environment, and then attempts
+ * to execute the command using execve. If execve fails, it writes the
+ * command name to the standard error, frees the allocated memory for
+ * the command arguments, and exits the process.
+ */
 void	ft_execve(char *cmd, char **env)
 {
 	char	**s_cmd;
@@ -34,6 +45,22 @@ void	ft_execve(char *cmd, char **env)
 	}
 }
 
+/**
+ * @brief Retrieves the full path of a command by searching through the 
+ * system's PATH environment variable.
+ *
+ * This function takes a command and the environment variables, splits the
+ *  PATH environment variable into
+ * individual paths, and checks each path to see if the command exists and 
+ * is executable. If the command is found,
+ * the full path to the command is returned. If the command is not found, 
+ * the original command is returned.
+ *
+ * @param cmd The command to search for.
+ * @param env The environment variables.
+ * @return The full path to the command if found, otherwise the original 
+ * command.
+ */
 char	*ft_my_getenv_(char *cmd, char **env)
 {
 	int		i;
@@ -62,6 +89,17 @@ char	*ft_my_getenv_(char *cmd, char **env)
 	return (cmd);
 }
 
+/**
+ * ft_my_getenv - Retrieves the value of an environment variable.
+ * @name: The name of the environment variable to find.
+ * @env: The array of environment variables.
+ *
+ * This function searches through the provided environment variables to find
+ * the one that matches the given name. If a match is found, it returns the
+ * value of the environment variable. If no match is found, it returns NULL.
+ *
+ * Return: The value of the environment variable if found, otherwise NULL.
+ */
 char	*ft_my_getenv(char *name, char **env)
 {
 	int		i;
