ca32d422b88069145c9d832ddbff3c4c8ecd46dd
diegonmarcos <diegonmarcos@gmail.com>
2025-10-28 15:20:27 +0100

    up

--- S1_Cloud/clasp/gmailFilter.js
+++
@@ -1,9 +0,0 @@
-/**
- * Creates a Gmail filter that archives emails from a specific sender.
- */
-function createGmailFilter() {
-  const filter = GmailApp.createFilter()
-    .from('example@example.com')
-    .archive();
-  Logger.log('Created filter: ' + filter.getId());
-}
